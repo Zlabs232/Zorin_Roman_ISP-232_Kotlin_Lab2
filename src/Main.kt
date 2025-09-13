c3eb05c6325e0ec7a951b7a4b3213595433d068f
Роман Зорин <sgena170315@gmail.com>
2025-09-13 16:37:46 +0300

    // Задание 1. Проверка уровня доступа игрока

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    //Task 1
+    print("Введите ваш уровень: ")
+    val level = readln().toInt()
+    val status = if (level < 10) "Новичок" else if (level < 50) "Опытный" else "Легенда"
+    val bonus = if (level < 10) 10 else if (level < 50) 30 else 100
+    println("Ваш статус: $status, вы получаете бонус +$bonus HP")
 
-    }
+}
